be0a4df82c23b9ceb16efcbea52ada0e8fdfa204
collinpowell <collinskrubu723@gmail.com>
2024-04-20 10:57:13 +0100

    Keys Confirmation

--- src/config/client.ts
+++ src/config/client.ts
@@ -2,7 +2,13 @@ import { RestClientV5 } from "bybit-api";
 
 const key = process.env.API_KEY;
 const secret = process.env.API_SECRET;
-const demo = process.env.DEMO == 'false' ? false : true;
+const demo = process.env.DEMO == "false" ? false : true;
+
+if (!key) {
+  throw new Error("Kindly Configure API Keys and Secrets");
+} else if (key.length < 1) {
+  throw new Error("Kindly Configure API Keys and Secrets");
+}
 
 const client = new RestClientV5({
   key: key,
@@ -11,4 +17,4 @@ const client = new RestClientV5({
   demoTrading: demo,
 });
 
-export default client
+export default client;
